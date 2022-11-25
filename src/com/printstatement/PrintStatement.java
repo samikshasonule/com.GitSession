1f69237f8d74099b6db9eab8f5b03a5743c94d44
unknown <samiksha.sonule1705@gmail.com>
2022-11-25 15:20:50 +0530

    This is my Third commit on second repository.

---
+++ src/com/printstatement/PrintStatement.java
@@ -0,0 +1,12 @@
+package com.printstatement;
+
+public class PrintStatement {
+	public static void main(String args[]) {
+    int i;
+    if(System.out.printf("Hello,Samiksha ")==null) {}
+    for(i=0;i<=10;System.out.println("Welcome to java")) {
+	   i++;
+     }
+   }
+}
+
